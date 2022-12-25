21e0517117bf0ff1404b80d256fbf737f1e433dc
Yato <yatoakii@gmail.com>
2022-12-25 21:36:54 +0630

    Can generate random meeting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
+import randomstring from 'randomstring';
 import Chat from "./components/Chat.js";
 import Login from "./components/Login.js";
 
 const App = () => {
     
     const [userLogin, setUserLogin] = useState(false);
+    const [meetingToken,setMeetingToken] = useState();
+
+    useEffect( () => {
+        setMeetingToken(randomstring.generate(16))
+    },[])
 
     const loginUser = () => {
         setUserLogin(true);
@@ -19,7 +25,7 @@ const App = () => {
                     )
                 }else{
                     return(
-                        <Login loginUser={loginUser}/>
+                        <Login loginUser={loginUser} meetingToken={meetingToken}/>
                     )
                 }
             })()}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Login = ({loginUser}) => {
+const Login = ({loginUser,meetingToken}) => {
 
     return(
         <div className="h-full flex items-center justify-center flex-col gap-5">
-            <input type="text" className="focus:outline-none bg-transparent py-2 px-8 text-center w-1/4 border border-white rounded-full"/>
-            <button onClick={loginUser} className="font-bold text-sm tracking-widest bg-blue-600 px-5 py-2 rounded-lg hover:bg-blue-700 transition-colors">Start the chat</button>
+            <input type="text" value={meetingToken} className="focus:outline-none bg-transparent py-2 px-8 text-center w-1/4 border border-white rounded-full"/>
+            <button onClick={loginUser} className="font-bold text-sm tracking-widest bg-blue-800 px-5 py-2 rounded-lg hover:bg-blue-600 transition-colors">Start the chat</button>
         </div>
     )
 }
