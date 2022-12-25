b4092d516122fe88a45aca65a1a1740a7c3727c9
Yato <yatoakii@gmail.com>
2022-12-25 21:05:11 +0630

    Added Login interface

--- src/App.js
+++ src/App.js
@@ -1,87 +1,28 @@
-import React from "react";
-const App = () => {
+import React,{useState} from "react";
+import Chat from "./components/Chat.js";
+import Login from "./components/Login.js";
 
+const App = () => {
+    
+    const [userLogin, setUserLogin] = useState(false);
+
+    const loginUser = () => {
+        setUserLogin(true);
+    }
 
     return(
-        <div className="text-xl">
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-
-            <br />Hi
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-            <br />Hi
-            <br />Hi
-            <br />Hi
-
-
-            <br />Hi
+        <div className="h-full">
+            {(() => {
+                if(userLogin === true){
+                    return (
+                        <Chat/>
+                    )
+                }else{
+                    return(
+                        <Login loginUser={loginUser}/>
+                    )
+                }
+            })()}
         </div>
     )
 }
---
+++ src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Chat = () => {
+
+    return(
+        <div>
+            <h1>Chat area</h1>
+        </div>
+    )
+}
+
+export default Chat;
