401d4d1ea7a7de8b4f25aae03d65e1f411a4b3c3
Yato <yatoakii@gmail.com>
2022-12-25 15:45:03 +0630

    Added Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",'./public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Montserrat: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+        Comfortaa: ['"Comfortaa"', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
