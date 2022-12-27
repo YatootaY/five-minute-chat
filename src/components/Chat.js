f19a955862c1a8325517e583af49837b4e29ca0e
Yato <yatoakii@gmail.com>
2022-12-27 13:40:17 +0630

    Added Msg area

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -7,12 +7,10 @@ const Chat = () => {
         <div className="flex flex-col h-full">
             <Messages/>
             
-            <div className="">
-                <form className="flex gap-3 md:px-40 px-20">
-                    <input className="text-xs w-full focus:outline-none bg-slate-800 py-2 px-5 ml-3 flex-grow rounded-md" id="msg" type="text" placeholder='Enter your message' autoComplete="off"/>
-                    <input type="submit" className="text-xs bg-blue-800 hover:bg-blue-600 px-3 rounded-md cursor-pointer"/>
-                </form>
-            </div>            
+            <form className="flex gap-3 md:px-40 px-20 mr-2">
+                <input className="text-xs focus:outline-none bg-slate-800 py-2 px-5 flex-grow rounded-md" id="msg" type="text" placeholder='Enter your message' autoComplete="off"/>
+                <button className="text-xs bg-blue-800 hover:bg-blue-600 px-3 rounded-md cursor-pointer">Send</button>
+            </form>          
         </div>
     )
 }
--- src/components/components/Messages.js
+++ src/components/components/Messages.js
@@ -3,48 +3,92 @@ import React from "react";
 const Messages = () => {
 
     return(
-        <div className="flex flex-col flex-grow overflow-auto">
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1><h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
+        <div className="flex flex-col flex-grow overflow-auto md:px-40 px-20 py-4">
+            <div className=" hover:bg-gray-900 p-2 rounded-lg cursor-default mt-3">
+                <div className="userInfo flex gap-4 ">
+                    <div className="userIcon text-3xl">😶</div>
+                    <div className="space-y-1">
+                        <h3 className="text-lg font-extrabold">Happy Boy</h3>
+                        <h4 className="text-xs text-slate-500">5:00</h4>
+                        <p>THis is nice. It is oadi oiasdjf oi</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className=" hover:bg-gray-900 p-2 rounded-lg cursor-default mt-3">
+                <div className="userInfo flex gap-4 ">
+                    <div className="userIcon text-3xl">😶</div>
+                    <div className="space-y-1">
+                        <h3 className="text-lg font-extrabold">Happy Boy</h3>
+                        <h4 className="text-xs text-slate-500">5:00</h4>
+                        <p>THis is nice. It is oadi oiasdjf oi</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className=" hover:bg-gray-900 p-2 rounded-lg cursor-default mt-3">
+                <div className="userInfo flex gap-4 ">
+                    <div className="userIcon text-3xl">😶</div>
+                    <div className="space-y-1">
+                        <h3 className="text-lg font-extrabold">Happy Boy</h3>
+                        <h4 className="text-xs text-slate-500">5:00</h4>
+                        <p>THis is nice. It is oadi oiasdjf oi</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className=" hover:bg-gray-900 p-2 rounded-lg cursor-default mt-3">
+                <div className="userInfo flex gap-4 ">
+                    <div className="userIcon text-3xl">😶</div>
+                    <div className="space-y-1">
+                        <h3 className="text-lg font-extrabold">Happy Boy</h3>
+                        <h4 className="text-xs text-slate-500">5:00</h4>
+                        <p>THis is nice. It is oadi oiasdjf oi</p>
+                    </div>
+                </div>
+            </div>
+            <div className=" hover:bg-gray-900 p-2 rounded-lg cursor-default mt-3">
+                <div className="userInfo flex gap-4 ">
+                    <div className="userIcon text-3xl">😶</div>
+                    <div className="space-y-1">
+                        <h3 className="text-lg font-extrabold">Happy Boy</h3>
+                        <h4 className="text-xs text-slate-500">5:00</h4>
+                        <p>THis is nice. It is oadi oiasdjf oi</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className=" hover:bg-gray-900 p-2 rounded-lg cursor-default mt-3">
+                <div className="userInfo flex gap-4 ">
+                    <div className="userIcon text-3xl">😶</div>
+                    <div className="space-y-1">
+                        <h3 className="text-lg font-extrabold">Happy Boy</h3>
+                        <h4 className="text-xs text-slate-500">5:00</h4>
+                        <p>THis is nice. It is oadi oiasdjf oi</p>
+                    </div>
+                </div>
+            </div>
+            <div className=" hover:bg-gray-900 p-2 rounded-lg cursor-default mt-3">
+                <div className="userInfo flex gap-4 ">
+                    <div className="userIcon text-3xl">😶</div>
+                    <div className="space-y-1">
+                        <h3 className="text-lg font-extrabold">Happy Boy</h3>
+                        <h4 className="text-xs text-slate-500">5:00</h4>
+                        <p>THis is nice. It is oadi oiasdjf oi</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className=" hover:bg-gray-900 p-2 rounded-lg cursor-default mt-3">
+                <div className="userInfo flex gap-4 ">
+                    <div className="userIcon text-3xl">😶</div>
+                    <div className="space-y-1">
+                        <h3 className="text-lg font-extrabold">Happy Boy</h3>
+                        <h4 className="text-xs text-slate-500">5:00</h4>
+                        <p>THis is nice. It is oadi oiasdjf oi</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
